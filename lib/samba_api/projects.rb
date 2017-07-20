61ceb345f9a84f3c9dfcf40075179203130cdbfe
leandrochili <lndr.figueredo@gmail.com>
2017-07-20 01:51:36 -0300

    Better method to prepare project endpoints

--- lib/samba_api/projects.rb
+++ lib/samba_api/projects.rb
@@ -10,11 +10,18 @@ module SambaApi
     end
 
     def get_projects
+      prepare_endpoint
+      response = self.class.get(prepare_endpoint)
+      JSON.parse(response.body)
+    end
+
+    private
+
+    def prepare_endpoint
       endpoint = SambaApi::BASE_URL
       namespace = SambaApi.demodulize_class(self.class)
-      access_token = "?access_token=#{@options["access_token"]}"
-      response = self.class.get(endpoint + namespace + access_token)
-      JSON.parse(response.body)
+      access_token = "?access_token=#{@options['access_token']}"
+      return endpoint + namespace + access_token
     end
   end
 end
