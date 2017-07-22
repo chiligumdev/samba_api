5a43e69164a6c216bfd48702dfe942bf74e58654
leandrochili <lndr.figueredo@gmail.com>
2017-07-21 21:43:44 -0300

    Projects endpoint without tests

--- lib/samba_api/projects.rb
+++ lib/samba_api/projects.rb
@@ -23,12 +23,17 @@ module SambaApi
       JSON.parse(response.body)
     end
 
-    def create_project
-      # SOMETHING IN THE WAY, UHHHMMMM HUMMMMM YEAH
+    def create_project(name, desc)
+      values = { name: name.to_s, description: desc.to_s }.to_json
+      endpoint_url = base_url + namespace + access_token
+      response = self.class.post(endpoint_url, values, header_json)
+      response.code
     end
 
     def delete_project(project_id)
-      # SOMETHING IN THE WAY, UHHHMMMM HUMMMMM YEAH
+      endpoint_url = base_url + namespace + '/' + project_id.to_s + access_token
+      response = self.class.delete(endpoint_url, header_json)
+      response.code
     end
 
     private
@@ -43,6 +48,10 @@ module SambaApi
 
     def access_token
       "?access_token=#{@options['access_token']}"
+    end
+
+    def header_json
+      'application/json'
     end
   end
 end
