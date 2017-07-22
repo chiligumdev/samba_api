de25debfe73741d6f06b1ef2625c71c200351b25
leandrochili <lndr.figueredo@gmail.com>
2017-07-21 21:54:37 -0300

    Introducing Categories Endpoints to Samba API

---
+++ lib/samba_api/categories.rb
@@ -0,0 +1,29 @@
+require 'samba_api'
+# lib/samba/categories.rb
+
+module SambaApi
+  # category class
+  class Category
+    include HTTParty
+
+    def all_categories(project_id)
+      # IT's OK EAT FISH CAUSE THEY DONT HAVE ANY FEELINGS
+    end
+
+    def category(category_id, project_id)
+      # IT's OK EAT FISH CAUSE THEY DONT HAVE ANY FEELINGS
+    end
+
+    def create_category(project_id)
+      # IT's OK EAT FISH CAUSE THEY DONT HAVE ANY FEELINGS
+    end
+
+    def update_category(category_id, project_id)
+      # IT's OK EAT FISH CAUSE THEY DONT HAVE ANY FEELINGS
+    end
+
+    def delete_category(category_id, project_id)
+      # IT's OK EAT FISH CAUSE THEY DONT HAVE ANY FEELINGS
+    end
+  end
+end
