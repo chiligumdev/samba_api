9317d4766a58f61a199e7a69dde4ca98129b6870
Lean <lndr.figueredo@gmail.com>
2017-08-10 10:28:51 -0300

    Adding simplecov to see covered modules

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,3 @@
 require 'rspec'
+require 'simplecov'
 require 'samba_api'
--- spec/spec_samba.rb
+++ spec/spec_samba.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe SambaApi::Projects do
   it "Should Be pass" do
-    expect(SambaApi::Projects.greet).to eq("Hello World") 
+    expect(SambaApi::Projects.greet).to eq("hello world") 
   end
 end
