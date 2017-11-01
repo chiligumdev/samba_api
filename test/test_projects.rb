610d8c19a2fff6755e63b59f322da0653dc80888
Lean <lndr.figueredo@gmail.com>
2017-11-01 14:31:51 -0200

    Clean Old Stuff

--- test/test_projects.rb
+++ test/test_projects.rb
@@ -3,13 +3,6 @@ require 'httparty'
 require 'samba_api/projects'
 
 class TestProjects < Minitest::Test
-  def setup
-    @projects = SambaApi::Projects.new(access_token: ENV['SAMBA_TOKEN'])
-    @invalid_projects = SambaApi::Projects.new(access_token: 'WRONG_TOKEN')
-  end
-
-  def test_get_all_projects
-    
-  end
+  
 end
 
