d57860a34a6e7bc29dc2f94ca6a494c5130636b8
Lean <lndr.figueredo@gmail.com>
2017-11-17 10:18:08 -0200

    Getting all medias endpoint

--- lib/samba_api/medias.rb
+++ lib/samba_api/medias.rb
@@ -1,8 +1,16 @@
 require 'samba_api'
+require 'open3'
+
 # lib/samba/medias.rb
 module SambaApi 
   # medias class
   module Medias
+
+    def all_medias project_id
+      endpoint_url = base_url + 'medias' + access_token + '&pid=' + project_id.to_s
+      response = self.class.get(endpoint_url)
+      JSON.parse(response.body)
+    end
     
   end
 end
--- lib/samba_api/version.rb
+++ lib/samba_api/version.rb
@@ -3,5 +3,5 @@ module SambaApi
   VERSION = '0.1.0'.freeze
   BASE_URL = 'http://api.sambavideos.sambatech.com/v1/'.freeze
   CATEGORY_BASE_URL = 'http://api.sambavideos.sambatech.com/v1/categories/'.freeze
-
+  MEDIA_BASE_URL = 'http://api.sambavideos.sambatech.com/v1/medias/'.freeze
 end
