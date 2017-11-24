f3e9041fa5370caf132fc096a2dc152b060b2181
Lean <lndr.figueredo@gmail.com>
2017-11-24 18:44:48 -0200

    New Version

--- lib/samba_api/version.rb
+++ lib/samba_api/version.rb
@@ -1,6 +1,6 @@
 module SambaApi
 
-  VERSION = '0.3.1'.freeze
+  VERSION = '0.4.1'.freeze
   BASE_URL = 'http://api.sambavideos.sambatech.com/v1/'.freeze
   CATEGORY_BASE_URL = 'http://api.sambavideos.sambatech.com/v1/categories/'.freeze
   MEDIA_BASE_URL = 'http://api.sambavideos.sambatech.com/v1/medias/'.freeze
